dedae38b1dc925e22eb2dd75f012d54a22155a8a
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2019-09-05 08:32:59 -0500

    update project

--- js/main.js
+++ js/main.js
@@ -249,6 +249,46 @@
             ]
         });
 
+
+        $('.cliente').slick({
+            arrows: false,
+            dots: true,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            autoplay: true,
+            pauseOnFocus: false,
+            autoplaySpeed: 3000,
+            responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+            ]
+        });
+
         $('.testimonials').slick({
             arrows: true,
             dots: false,
