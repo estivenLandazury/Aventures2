9b2d3af920199cb1bb50e1d9773c695dfd3efdd3
estivenLandazury <estiven.landazury@correo.icesi.edu.co>
2019-10-31 12:00:19 -0500

    Grace Update

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,14 +1,22 @@
+const spinner = document.getElementById("spinner");
 
 document.getElementById('subscribe').addEventListener('click', restCall);
 function restCall() {
     var email = document.getElementById("mc-email").value;
+    var name = document.getElementById("name").value;
+    var Company = document.getElementById("company").value;
+    var location = document.getElementById("location").value;
+    var Objective = document.getElementById("objective").value;
+
+
+
 
     let Data =
     {
         "toEmails": [
             "cts.prescriptiva@carvajal.com"],
         "subject": "Ventures - Contacto",
-        "message": "Deseo que se contacte con nosotros por favor,  mi email es el siguiente " + email
+        "message": "Deseo que se contacte con nosotros, mi compañia se llama " + Company + " Mi nombre es " + name + " Mi Correo electrónico es  " + email + " Y estamos ubicados en " + location + " Objetivo: " + Objective
     }
 
 
@@ -22,18 +30,41 @@ function restCall() {
         body: JSON.stringify(Data)
     }
 
-    if (email == "") {
-        location.reload();
+    if (email == "" || name == "" || Company == "" || location == "" || Objective == "") {
+
+        console.log("No se puede enviar mensaje ")
+        
+        message()
     } else {
+        spinner.removeAttribute('hidden');
+
         fetch('https://0gqxxhb0wb.execute-api.us-east-1.amazonaws.com/Prod/send/', options)
             .then(function (response) {
-                $('#exampleModal').modal('show');
+                /* $('#exampleModal').modal('show');*/
+                console.log(response.status)
+                message1()
+                spinner.setAttribute('hidden', '');
+
             })
     }
 
 
 
 
+}
+
+function message() {
+    window.alert("Ingrese los campos requeridos")
+}
+
+function message1() {
+    window.alert("Menssaje Enviado Correctamente")
+
+    var email = document.getElementById("mc-email").value="";
+    var name = document.getElementById("name").value="";
+    var Company = document.getElementById("company").value="";
+    var location = document.getElementById("location").value="";
+    var Objective = document.getElementById("objective").value="";
 }
 
 
